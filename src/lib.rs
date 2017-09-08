4ee76182775fc96509448ec20977480d27c09aed
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-09-08 18:02:42 +0900

    added Queue Family operation helpers

--- src/lib.rs
+++ src/lib.rs
@@ -28,14 +28,16 @@ impl VkResultHandler for VkResult
 	fn into_result(self) -> Result<()> { if self == VK_SUCCESS { Ok(()) } else { Err(self) } }
 }
 
+/// Opaque handle to a instance object
 pub struct Instance(VkInstance);
+/// Opaque handle to a physical device object
 pub struct PhysicalDevice(VkPhysicalDevice);
+/// Builder object for constructing a `Instance`
 pub struct InstanceBuilder
 {
 	app_name: CString, engine_name: CString, extensions: Vec<CString>, layers: Vec<CString>,
 	appinfo: VkApplicationInfo, cinfo: VkInstanceCreateInfo
 }
-#[cfg(feature = "FeImplements")]
 impl InstanceBuilder
 {
 	pub fn new(app_name: &str, app_version: (u32, u32, u32), engine_name: &str, engine_version: (u32, u32, u32)) -> Self
@@ -69,6 +71,7 @@ impl InstanceBuilder
 		let mut s = self;
 		for l in layers { s = s.add_layer(l); } s
 	}
+	#[cfg(feature = "FeImplements")]
 	pub fn create(self) -> Result<Instance>
 	{
 		let app = VkApplicationInfo
@@ -156,7 +159,7 @@ impl PhysicalDevice
 	/// Lists physical device's format capabilities
 	pub fn format_properties(&self, format: VkFormat) -> VkFormatProperties
 	{
-		let mut p = unsafe { std::mem::uninitialize() };
+		let mut p = unsafe { std::mem::uninitialized() };
 		unsafe { vkGetPhysicalDeviceFormatProperties(self.0, format, &mut p) }; p
 	}
 	/// Lists physical device's image format capabilities
@@ -194,3 +197,46 @@ impl PhysicalDevice
 	}
 }
 
+/// Set of bit of queue flags
+#[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub struct QueueFlags(VkQueueFlags);
+impl QueueFlags
+{
+	/// Empty Bits
+	pub fn new() -> Self { QueueFlags(0) }
+	/// Supports only graphics operations
+	pub fn graphics1() -> Self { QueueFlags(VK_QUEUE_GRAPHICS_BIT) }
+	/// Supports only compute operations
+	pub fn compute1() -> Self { QueueFlags(VK_QUEUE_COMPUTE_BIT) }
+	/// Supports only transfer operations
+	pub fn transfer1() -> Self { QueueFlags(VK_QUEUE_TRANSFER_BIT) }
+	/// Supports only sparse memory management operations
+	pub fn sparse_binding1() -> Self { QueueFlags(VK_QUEUE_SPARSE_BINDING_BIT) }
+	/// Supports graphics operations
+	pub fn graphics(self) -> Self { QueueFlags(self.0 | VK_QUEUE_GRAPHICS_BIT) }
+	/// Supports compute operations
+	pub fn compute(self) -> Self { QueueFlags(self.0 | VK_QUEUE_COMPUTE_BIT) }
+	/// Supports transfer operations
+	pub fn transfer(self) -> Self { QueueFlags(self.0 | VK_QUEUE_TRANSFER_BIT) }
+	/// Supports sparse memory management operatinons
+	pub fn sparse_binding(self) -> Self { QueueFlags(self.0 | VK_QUEUE_SPARSE_BINDING_BIT) }
+}
+/// List of queue families
+pub struct QueueFamilies(Vec<VkQueueFamilyProperties>);
+impl QueueFamilies
+{
+	/// Find a queue family index containing specified bitflags
+	#[allow(non_snake_case)]
+	pub fn find_matching_index(&self, flags: QueueFlags) -> Option<u32>
+	{
+		self.0.iter().enumerate().find(|&(_, &VkQueueFamilyProperties { queueFlags, .. })| (queueFlags & flags.0) != 0).map(|(n, _)| n as _)
+	}
+	/// Number of queue families
+	pub fn count(&self) -> u32 { self.0.len() as _ }
+	/// Number of queues in selected queue family
+	pub fn queue_count(&self, family_index: u32) -> u32 { self.0[family_index as usize].queueCount }
+	/// Unsigned integer count of meaningful bits in the timestamps written via `vkCmdWriteTimestamp`
+	pub fn timestamp_valid_bits(&self, family_index: u32) -> u32 { self.0[family_index as usize].timestampValidBits }
+	/// Minimum granularity supported for image transfer operations on the queues in selected queue family
+	pub fn minimum_image_transfer_granularity(&self, family_index: u32) -> &VkExtent3D { &self.0[family_index as usize].minImageTransferGranularity }
+}
