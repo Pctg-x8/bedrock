cfd88b9da1d8b29ec6fd2e5d7372997255adcf43
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-09-11 18:41:58 +0900

    move Sampler to resources

--- src/resources.rs
+++ src/resources.rs
@@ -548,3 +548,144 @@ impl AspectMask
 	/// The metadata aspect, used for sparse sparse resource oeprations
 	pub fn metadata(&self) -> Self { AspectMask(self.0 | Self::METADATA.0) }
 }
+
+/// Opaque handle to a sampler object
+pub struct Sampler(pub VkSampler, ::Device);
+#[cfg(feature = "FeImplements")] DeviceChildCommonDrop!{ for Sampler }
+
+/// Specify behavior of sampling with texture coordinates outside an image
+#[repr(C)] #[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub enum AddressingMode
+{
+    /// The repeat wrap mode
+    Repeat = VK_SAMPLER_ADDRESS_MODE_REPEAT as _,
+    /// The mirrored repeat wrap mode
+    MirroredRepeat = VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT as _,
+    /// The clamp to edge wrap mode
+    ClampToEdge = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE as _,
+    /// The clamp to border wrap mode
+    ClampToBorder = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER as _,
+    /// The mirror clamp to edge wrap mode
+    #[cfg(feature = "VK_KHR_mirror_clamp_to_edge")]
+    MirrorClampToEdge = VK_SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE as _
+}
+/// Specify filter used for texture lookups
+#[repr(C)] #[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub enum FilterMode
+{
+    /// Nearest filtering
+    Nearest = VK_FILTER_NEAREST as _,
+    /// Linear filtering
+    Linear = VK_FILTER_LINEAR as _
+}
+/// Specify mipmap mode used for texture lookups
+#[repr(C)] #[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub enum MipmapFilterMode
+{
+    /// Nearest filtering
+    Nearest = VK_SAMPLER_MIPMAP_MODE_NEAREST as _,
+    /// Linear filtering
+    Linear = VK_SAMPLER_MIPMAP_MODE_LINEAR as _
+}
+/// Specify border color used for texture lookups
+#[repr(C)] #[derive(Debug, Clone, PartialEq, Eq, Copy)]
+pub enum BorderColor
+{
+    /// A transparent, floating-point format, black color
+    TransparentBlackF = VK_BORDER_COLOR_FLOAT_TRANSPARENT_BLACK as _,
+    /// A transparent, integer format, black color
+    TransparentBlackI = VK_BORDER_COLOR_INT_TRANSPARENT_BLACK as _,
+    /// An opaque, floating-point format, black color
+    OpaqueBlackF = VK_BORDER_COLOR_FLOAT_OPAQUE_BLACK as _,
+    /// An opaque, integer format, black color
+    OpaqueBlackI = VK_BORDER_COLOR_INT_OPAQUE_BLACK as _,
+    /// An opaque, floating-point format, white color
+    OpaqueWhiteF = VK_BORDER_COLOR_FLOAT_OPAQUE_WHITE as _,
+    /// An opaque, integer format, white color
+    OpaqueWhiteI = VK_BORDER_COLOR_INT_OPAQUE_WHITE as _
+}
+/// Builder object for constructing the sampler object
+pub struct SamplerBuilder(VkSamplerCreateInfo);
+impl SamplerBuilder
+{
+    pub fn new() -> Self
+    {
+        SamplerBuilder(VkSamplerCreateInfo
+        {
+            magFilter: FilterMode::Linear as _, minFilter: FilterMode::Linear as _, mipmapMode: MipmapFilterMode::Linear as _,
+            addressModeU: AddressingMode::Repeat as _, addressModeV: AddressingMode::Repeat as _, addressModeW: AddressingMode::Repeat as _,
+            mipLodBias: 0.0, anisotropyEnable: false as _, compareEnable: false as _, compareOp: ::CompareOp::Always as _,
+            minLod: 0.0, maxLod: 0.0, borderColor: BorderColor::TransparentBlackF as _, unnormalizedCoordinates: false as _,
+            .. Default::default()
+        })
+    }
+    /// The magnification and the minification filters to apply to lookups.
+    /// Default: Magnification=`FilterMode::Linear`, Minification=`FilterMode::Linear`
+    pub fn filter(&mut self, mag: FilterMode, min: FilterMode) -> &mut Self
+    {
+        self.0.magFilter = mag as _; self.0.minFilter = min as _; self
+    }
+    /// The mipmap filter to apply to lookups. Default: `MipmapFilterMode::Linear`
+    pub fn mip_filter(&mut self, f: MipmapFilterMode) -> &mut Self
+    {
+        self.0.mipmapMode = f as _; self
+    }
+    /// The addressing mode for outside [0..1] range for U, V and W coordinates.
+    /// Default: U=`AddressingMode::Repeat`, V=`AddressinMode::Repeat`, W=`AddressingMode::Repeat`
+    pub fn addressing(&mut self, u: AddressingMode, v: AddressingMode, w: AddressingMode) -> &mut Self
+    {
+        self.0.addressModeU = u as _; self.0.addressModeV = v as _; self.0.addressModeW = w as _; self
+    }
+    /// The bias to be added to mipmap LOD calculation and bias provided by image sampling functions in SPIR-V,
+    /// as described in the `Level-of-Detail Operation` section in Vulkan Specification.
+    /// Default: 0.0
+    pub fn lod_bias(&mut self, bias: f32) -> &mut Self { self.0.mipLodBias = bias; self }
+    /// The anisotropy value clamp. Specifying `None` switches off the anisotropic filtering
+    /// Default: `None`
+    pub fn max_anisotropy(&mut self, level: Option<f32>) -> &mut Self
+    {
+        self.0.anisotropyEnable = level.is_some() as _;
+        self.0.maxAnisotropy = level.unwrap_or_default(); self
+    }
+    /// The comparison function to apply to fetched data before filtering
+    /// as described in the `Depth Compare Operation` section in Vulkan Specification.
+    /// Specifying `None` switches off the comparison against a reference value during lookups.
+    /// Default: `None`
+    pub fn comparison(&mut self, op: Option<::CompareOp>) -> &mut Self
+    {
+        self.0.compareEnable = op.is_some() as _;
+        self.0.compareOp = op.unwrap_or(::CompareOp::Always) as _; self
+    }
+    /// The values used to clamp the computed level-of-detail value,
+    /// as described in the `Level-of-Detail Operation` section in Vulkan Specification.
+    /// Default: min_lod=0.0, max_lod=0.0
+    /// # Panics
+    /// `max_lod` must be greater than or equal to `min_lod`
+    pub fn lod_clamp(&mut self, min_lod: f32, max_lod: f32) -> &mut Self
+    {
+        assert!(max_lod >= min_lod);
+        self.0.minLod = min_lod; self.0.maxLod = max_lod; self
+    }
+    /// Whether to use unnormalized or normalized texel coordinates to address texels of the image.
+    /// Default: `false`
+    /// # Safety
+    /// User must meet the constraints as described in the "Valid Usage" section in the `VkSamplerCreateInfo` manual page
+    pub unsafe fn unnormalized_coordinates(&mut self, use_unnormalized: bool) -> &mut Self
+    {
+        self.0.unnormalizedCoordinates = use_unnormalized as _; self
+    }
+
+    /// Create a new sampler object
+    /// # Failures
+    /// On failure, this command returns
+    /// - VK_ERROR_OUT_OF_HOST_MEMORY
+    /// - VK_ERROR_OUT_OF_DEVICE_MEMORY
+    /// - VK_ERROR_TOO_MANY_OBJECTS
+    #[cfg(feature = "FeImplements")]
+    pub fn create(&self, device: &Device) -> Result<Sampler>
+    {
+        let mut h = VK_NULL_HANDLE as _;
+        unsafe { vkCreateSampler(device.native_ptr(), &self.0, std::ptr::null(), &mut h) }
+            .into_result().map(|_| Sampler(h, device.clone()))
+    }
+}
