781741a5ed101ed3d383e169c96a91ac58064ef9
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-10-07 14:04:58 +0900

    update docs

--- docs/implementors/compiler_builtins/float/trait.Float.js
+++ docs/implementors/compiler_builtins/float/trait.Float.js
@@ -1,7 +1,5 @@
 (function() {var implementors = {};
-implementors["dlib"] = [];
 implementors["libc"] = [];
-implementors["libloading"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
