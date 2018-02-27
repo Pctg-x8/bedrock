21b7b18b98bb4306ae53145c9e7c67810fcd6e0c
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-02-27 19:18:42 +0900

    fixed moltenvk linking

--- src/vk/mod.rs
+++ src/vk/mod.rs
@@ -2588,7 +2588,8 @@ pub type PFN_vkCmdExecuteCommands        = extern "system" fn(commandBuffer: VkC
 #[cfg(feature = "FeImplements")]
 #[cfg_attr(all(not(windows), not(target_os="macos")), link(name = "vulkan"))]
 #[cfg_attr(windows, link(name = "vulkan-1"))]
-#[cfg_attr(target_os="macos", link(name = "vulkan", kind = "framework"))]
+#[cfg_attr(target_os="macos", link(name = "MoltenVK", kind = "framework"))]
+#[cfg_attr(target_os="macos", link(name = "Metal", kind = "framework"))]
 extern "system"
 {
     pub fn vkCreateInstance(pCreateInfo: *const VkInstanceCreateInfo, pAllocator: *const VkAllocationCallbacks, pInstance: *mut VkInstance) -> VkResult;
