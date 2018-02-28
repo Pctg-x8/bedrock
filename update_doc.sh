5c274e05ca785811fb365556df04d33563707655
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-03-01 04:37:12 +0900

    add: travis deployment

--- update_doc.sh
+++ update_doc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mkdir -p docs
 cargo doc --features=FeImplements,FePresentation,VK_EXT_debug_report && rsync --delete -auv target/doc/ docs
 mkdir docs/ja && rsync --delete -auv target/doc/ docs/ja && node translate_ja.js
