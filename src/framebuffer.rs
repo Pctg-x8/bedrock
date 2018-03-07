7990dd6ebed1194f4789591a3ca95506eee9b933
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-03-07 22:53:30 +0900

    add: attachment description constructor and modifying RenderPassBuilder

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -3,6 +3,9 @@
 use vk::*;
 use {VkHandle, DeviceChild};
 #[cfg(feature = "FeImplements")] use VkResultHandler;
+use std::borrow::{Borrow, BorrowMut};
+use std::mem::transmute;
+use ImageLayout;
 
 /// Opaque handle to a render pass object
 pub struct RenderPass(VkRenderPass, ::Device);
@@ -17,6 +20,85 @@ impl VkHandle for Framebuffer { type Handle = VkFramebuffer; fn native_ptr(&self
 impl DeviceChild for RenderPass { fn device(&self) -> &::Device { &self.1 } }
 impl DeviceChild for Framebuffer { fn device(&self) -> &::Device { &self.1 } }
 
+/// Builder structure to construct the `VkAttachmentDescription`
+pub struct AttachmentDescription(VkAttachmentDescription);
+impl AttachmentDescription
+{
+	pub fn new(format: VkFormat, init_layout: ImageLayout, fin_layout: ImageLayout) -> Self
+	{
+		AttachmentDescription(VkAttachmentDescription
+		{
+			format, samples: 1,
+			loadOp: VK_ATTACHMENT_LOAD_OP_DONT_CARE, storeOp: VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			stencilLoadOp: VK_ATTACHMENT_LOAD_OP_DONT_CARE, stencilStoreOp: VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			initialLayout: init_layout as _, finalLayout: fin_layout as _, flags: 0
+		})
+	}
+	pub fn format(mut self, fmt: VkFormat) -> Self { self.0.format = fmt; self }
+	pub fn load_op(mut self, op: LoadOp) -> Self { self.0.loadOp = op as _; self }
+	pub fn store_op(mut self, op: StoreOp) -> Self { self.0.storeOp = op as _; self }
+	pub fn stencil_load_op(mut self, op: LoadOp) -> Self { self.0.stencilLoadOp = op as _; self }
+	pub fn stencil_store_op(mut self, op: StoreOp) -> Self { self.0.stencilStoreOp = op as _; self }
+	pub fn init_layout(mut self, layout: ImageLayout) -> Self { self.0.initialLayout = layout as _; self }
+	pub fn fin_layout(mut self, layout: ImageLayout) -> Self { self.0.finalLayout = layout as _; self }
+	pub fn may_alias_attachment(mut self) -> Self { self.0.flags |= VK_ATTACHMENT_DESCRIPTION_MAY_ALIAS_BIT; self }
+	pub fn no_alias_attachment(mut self) -> Self { self.0.flags &= !VK_ATTACHMENT_DESCRIPTION_MAY_ALIAS_BIT; self }
+
+	pub fn mod_format(&mut self, fmt: VkFormat) -> &mut Self { self.0.format = fmt; self }
+	pub fn mod_load_op(&mut self, op: LoadOp) -> &mut Self { self.0.loadOp = op as _; self }
+	pub fn mod_store_op(&mut self, op: StoreOp) -> &mut Self { self.0.storeOp = op as _; self }
+	pub fn mod_stencil_load_op(&mut self, op: LoadOp) -> &mut Self { self.0.stencilLoadOp = op as _; self }
+	pub fn mod_stencil_store_op(&mut self, op: StoreOp) -> &mut Self { self.0.stencilStoreOp = op as _; self }
+	pub fn mod_init_layout(&mut self, layout: ImageLayout) -> &mut Self { self.0.initialLayout = layout as _; self }
+	pub fn mod_fin_layout(&mut self, layout: ImageLayout) -> &mut Self { self.0.finalLayout = layout as _; self }
+	pub fn mod_may_alias_attachment(&mut self) -> &mut Self { self.0.flags |= VK_ATTACHMENT_DESCRIPTION_MAY_ALIAS_BIT; self }
+	pub fn mod_no_alias_attachment(&mut self) -> &mut Self { self.0.flags &= !VK_ATTACHMENT_DESCRIPTION_MAY_ALIAS_BIT; self }
+}
+impl Borrow<AttachmentDescription> for VkAttachmentDescription
+{
+	fn borrow(&self) -> &AttachmentDescription { unsafe { transmute(self) } }
+}
+impl BorrowMut<AttachmentDescription> for VkAttachmentDescription
+{
+	fn borrow_mut(&mut self) -> &mut AttachmentDescription { unsafe { transmute(self) } }
+}
+#[repr(C)] #[derive(Debug, Clone, Copy, PartialEq, Eq)] pub enum LoadOp
+{
+	/// The previous contents of the image within the render area will be preserved.
+	/// 
+	/// ## Used access types
+	/// 
+	/// This operation uses the "Read" access
+	Load = VK_ATTACHMENT_LOAD_OP_LOAD as _,
+	/// The contents within the render area will be cleared to a uniform value, which is
+	/// specified when a render pass instance is begun.
+	/// 
+	/// ## Used access types
+	/// 
+	/// This operation uses the "Write" access
+	Clear = VK_ATTACHMENT_LOAD_OP_CLEAR as _,
+	/// The previous contents within the area need not be preserved;
+	/// the contents of the attachment will be undefined inside the render area.
+	/// 
+	/// ## Used access types
+	/// 
+	/// This operation uses the "Write" access
+	DontCare = VK_ATTACHMENT_LOAD_OP_DONT_CARE as _
+}
+/// Possible argument values of `AttachmentDescription::store_op` and `stencil_store_op`,
+/// specifying how the contents of the attachment are treated.
+/// 
+/// ## Used access types
+/// 
+/// Both items use the "Write" access
+#[repr(C)] #[derive(Debug, Clone, Copy, PartialEq, Eq)] pub enum StoreOp
+{
+	/// The contents generated during the render pass and within the render area are written to memory.
+	Store = VK_ATTACHMENT_STORE_OP_STORE as _,
+	/// The contents within the render area are not needed after rendering, and *may* be discarded;
+	/// the contents of the attachment will be undefined inside the render area.
+	DontCare = VK_ATTACHMENT_STORE_OP_DONT_CARE as _
+}
 /// Builder structure to construct the `VkSubpassDescription`
 /// 
 /// ## The `layout` parameter of each attachment
@@ -48,10 +130,10 @@ pub struct RenderPassBuilder
 impl RenderPassBuilder
 {
 	pub fn new() -> Self { RenderPassBuilder { attachments: Vec::new(), subpasses: Vec::new(), dependencies: Vec::new() } }
-	pub fn add_attachment(&mut self, desc: VkAttachmentDescription) -> &mut Self { self.attachments.push(desc); self }
+	pub fn add_attachment(&mut self, desc: AttachmentDescription) -> &mut Self { self.attachments.push(desc.0); self }
 	pub fn add_subpass(&mut self, desc: SubpassDescription) -> &mut Self { self.subpasses.push(desc); self }
 	pub fn add_dependency(&mut self, desc: VkSubpassDependency) -> &mut Self { self.dependencies.push(desc); self }
-	pub fn add_attachments<Collection: IntoIterator<Item = VkAttachmentDescription>>(&mut self, collection: Collection) -> &mut Self
+	pub fn add_attachments<Collection: IntoIterator<Item = AttachmentDescription>>(&mut self, collection: Collection) -> &mut Self
 	{
 		for d in collection { self.add_attachment(d); } self
 	}
@@ -63,6 +145,10 @@ impl RenderPassBuilder
 	{
 		for d in collection { self.add_dependency(d); } self
 	}
+
+	pub fn mod_attachment(&mut self, index: usize) -> &mut AttachmentDescription { self.attachments[index].borrow_mut() }
+	pub fn mod_subpass(&mut self, index: usize) -> &mut SubpassDescription { &mut self.subpasses[index] }
+	pub fn mod_dependency(&mut self, index: usize) -> &mut VkSubpassDependency { &mut self.dependencies[index] }
 }
 impl SubpassDescription
 {
@@ -74,11 +160,11 @@ impl SubpassDescription
 			preserve_attachments: Vec::new()
 		}
 	}
-	pub fn add_input(mut self, index: u32, layout: ::ImageLayout) -> Self
+	pub fn add_input(mut self, index: u32, layout: ImageLayout) -> Self
 	{
 		self.input_attachments.push(VkAttachmentReference { attachment: index, layout: layout as _ }); self
 	}
-	pub fn add_color_output(mut self, index: u32, layout: ::ImageLayout, resolve: Option<(u32, ::ImageLayout)>) -> Self
+	pub fn add_color_output(mut self, index: u32, layout: ImageLayout, resolve: Option<(u32, ::ImageLayout)>) -> Self
 	{
 		if let Some((i, l)) = resolve
 		{
@@ -91,7 +177,7 @@ impl SubpassDescription
 		self.color_attachments.push(VkAttachmentReference { attachment: index, layout: layout as _ });
 		self
 	}
-	pub fn depth_stencil(mut self, index: u32, layout: ::ImageLayout) -> Self
+	pub fn depth_stencil(mut self, index: u32, layout: ImageLayout) -> Self
 	{
 		self.depth_stencil_attachment = Some(VkAttachmentReference { attachment: index, layout: layout as _ }); self
 	}
@@ -101,7 +187,37 @@ impl SubpassDescription
 	}
 	pub fn add_preserves<Collection: IntoIterator<Item = u32>>(mut self, collection: Collection) -> Self
 	{
-		for i in collection { self = self.add_preserve(i); } self
+		for i in collection { self.add_preserve_borrow(i); } self
+	}
+
+	pub fn add_input_borrow(&mut self, index: u32, layout: ImageLayout) -> &mut Self
+	{
+		self.input_attachments.push(VkAttachmentReference { attachment: index, layout: layout as _ }); self
+	}
+	pub fn add_color_output_borrow(&mut self, index: u32, layout: ::ImageLayout, resolve: Option<(u32, ::ImageLayout)>) -> &mut Self
+	{
+		if let Some((i, l)) = resolve
+		{
+			while self.resolve_attachments.len() < self.color_attachments.len()
+			{
+				self.resolve_attachments.push(VkAttachmentReference { attachment: VK_ATTACHMENT_UNUSED, layout: 0 as _ });
+			}
+			self.resolve_attachments.push(VkAttachmentReference { attachment: i, layout: l as _ });
+		}
+		self.color_attachments.push(VkAttachmentReference { attachment: index, layout: layout as _ });
+		self
+	}
+	pub fn depth_stencil_borrow(&mut self, index: u32, layout: ::ImageLayout) -> &mut Self
+	{
+		self.depth_stencil_attachment = Some(VkAttachmentReference { attachment: index, layout: layout as _ }); self
+	}
+	pub fn add_preserve_borrow(&mut self, index: u32) -> &mut Self
+	{
+		self.preserve_attachments.push(index); self
+	}
+	pub fn add_preserves_borrow<Collection: IntoIterator<Item = u32>>(&mut self, collection: Collection) -> &mut Self
+	{
+		for i in collection { self.add_preserve_borrow(i); } self
 	}
 }
 #[cfg(feature = "FeImplements")]
