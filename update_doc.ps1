4a75223acae06a297e5c9e98323a017a18038e6d
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-04-09 21:35:00 +0900

    fix: test scripts

--- update_doc.ps1
+++ update_doc.ps1
@@ -1,4 +1,4 @@
-cargo doc --features=FeImplements,FePresentation,VK_EXT_debug_report
+cargo doc --features=Implements,Presentation,VK_EXT_debug_report
 if($?) { robocopy target\doc docs /mir }
 mkdir docs/ja
 robocopy target\doc docs\ja /mir
