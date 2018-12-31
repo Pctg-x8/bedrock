62339c727dd7b5dc56991116835652ea365ec328
S.Percentage <Syn.Tri.Naga@gmail.com>
2018-12-31 17:42:04 +0900

    fix; path for absolute

--- src/vkresolve.rs
+++ src/vkresolve.rs
@@ -62,9 +62,12 @@ impl Resolver {
 
     #[cfg(feature = "DynamicLoaded")]
     fn new() -> Self {
-        #[cfg(target_os="macos")] const LIBNAME: &'static str = "./libvulkan.dylib";
-        #[cfg(not(target_os="macos"))] const LIBNAME: &'static str = "libvulkan.so";
-        Library::new(LIBNAME).map(Resolver).expect("Unable to open libvulkan.so")
+        #[cfg(target_os="macos")] const fn libname() -> String {
+            let mut exepath = std::env::current_exe(); exepath.pop();
+            exepath.push("libvulkan.dylib"); return exepath;
+        }
+        #[cfg(not(target_os="macos"))] const fn libname() -> &'static str { "libvulkan.so" }
+        Library::new(&libname()).map(Resolver).expect("Unable to open libvulkan.so")
     }
     #[cfg(not(feature = "DynamicLoaded"))]
     fn new() -> Self { Resolver() }
