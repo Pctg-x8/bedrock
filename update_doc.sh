49758c87585159d4ef63ae3a5a217d427d376b87
S.Percentage <Syn.Tri.Naga@gmail.com>
2019-05-05 18:51:43 +0900

    fix: doc without deps

--- update_doc.sh
+++ update_doc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p docs
-cargo doc --features=Implements,Presentation,VK_EXT_debug_report && rsync --delete -auv target/doc/ docs
+cargo doc --no-deps --features=Implements,Presentation,VK_EXT_debug_report && rsync --delete -auv target/doc/ docs
 mkdir docs/ja && rsync --delete -auv target/doc/ docs/ja && node translate_ja.js
