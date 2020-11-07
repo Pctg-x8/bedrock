4c64eb2f320af0811360e0cf2bc6360d742e4065
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-11-08 01:54:07 +0900

    fix: trailing slash

--- .github/actions/deployment-doc-peridot/gen.sh
+++ .github/actions/deployment-doc-peridot/gen.sh
@@ -3,6 +3,6 @@
 # Copy generated docs for deployment
 cd /doc
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public
-rsync --delete -auv $GITHUB_WORKSPACE/target/doc public/ja
+rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public/ja
 node /translate_ja.js
 firebase deploy --token $FIREBASE_TOKEN --project docs-541f3 --only hosting:peridot-branch
