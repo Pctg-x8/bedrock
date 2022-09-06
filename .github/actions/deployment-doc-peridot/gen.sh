9fb65aa6d59dc98ed560701f41b9dc2cfcc9df7f
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-09-07 01:27:02 +0900

    rm: debug output

--- .github/actions/deployment-doc-peridot/gen.sh
+++ .github/actions/deployment-doc-peridot/gen.sh
@@ -5,5 +5,4 @@ cd /doc
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public/ja
 node /translate_ja.js
-wc $GOOGLE_APPLICATION_CREDENTIALS
-firebase deploy --debug --project docs-541f3 --only hosting:peridot-branch
+firebase deploy --project docs-541f3 --only hosting:peridot-branch
