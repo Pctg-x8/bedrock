8809ae6216725d42487c1ae8e4924e2d6c6235be
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-09-06 20:44:29 +0900

    fix: dump env

--- .github/actions/deployment-doc-peridot/gen.sh
+++ .github/actions/deployment-doc-peridot/gen.sh
@@ -5,4 +5,5 @@ cd /doc
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public/ja
 node /translate_ja.js
+echo $GOOGLE_APPLICATION_CREDENTIALS
 firebase deploy --project docs-541f3 --only hosting:peridot-branch
