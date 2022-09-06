3af04736c2b9fd26210111a2d7e4a23352665bd6
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-09-06 22:27:16 +0900

    fix: add login

--- .github/actions/deployment-doc-peridot/gen.sh
+++ .github/actions/deployment-doc-peridot/gen.sh
@@ -5,5 +5,5 @@ cd /doc
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public/ja
 node /translate_ja.js
-echo $GOOGLE_APPLICATION_CREDENTIALS
+firebase login
 firebase deploy --project docs-541f3 --only hosting:peridot-branch
