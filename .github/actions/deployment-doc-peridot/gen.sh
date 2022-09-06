0263ed5fcb5d166c5393e5c8b4bf9074ed34a7f2
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-09-06 19:51:29 +0900

    rm: token args

--- .github/actions/deployment-doc-peridot/gen.sh
+++ .github/actions/deployment-doc-peridot/gen.sh
@@ -5,4 +5,4 @@ cd /doc
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public
 rsync --delete -auv $GITHUB_WORKSPACE/target/doc/ public/ja
 node /translate_ja.js
-firebase deploy --token $FIREBASE_TOKEN --project docs-541f3 --only hosting:peridot-branch
+firebase deploy --project docs-541f3 --only hosting:peridot-branch
