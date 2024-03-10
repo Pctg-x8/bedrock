b6c3523512d11811efc9f8d9e626b34a6a668054
S.Percentage <Syn.Tri.Naga@gmail.com>
2024-03-10 20:36:34 +0900

    fix: use core functionalities

--- src/ffi_helper.rs
+++ src/ffi_helper.rs
@@ -2,7 +2,7 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct FixedCStrBuffer<const L: usize>([u8; L]);
 impl<const L: usize> FixedCStrBuffer<L> {
-    pub const fn as_cstr(&self) -> Result<&std::ffi::CStr, core::ffi::FromBytesUntilNulError> {
+    pub const fn as_cstr(&self) -> Result<&core::ffi::CStr, core::ffi::FromBytesUntilNulError> {
         core::ffi::CStr::from_bytes_until_nul(&self.0)
     }
 }
