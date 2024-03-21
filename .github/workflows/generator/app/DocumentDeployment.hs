7258724264bcdd82b94bc86e303398c93abb7f3f
S.Percentage <Syn.Tri.Naga@gmail.com>
2024-03-21 09:18:20 +0900

    fix: use action

--- .github/workflows/generator/app/DocumentDeployment.hs
+++ .github/workflows/generator/app/DocumentDeployment.hs
@@ -1,8 +1,9 @@
 module DocumentDeployment (step) where
 
+import qualified Data.Map as M
 import qualified Workflow.GitHub.Actions as GHA
 
 step :: GHA.Step
 step =
   GHA.namedAs "Deployment to Firebase Hosting (for Peridot branch)" $
-    GHA.runStep "./.github/actions/deployment-doc-peridot"
+    GHA.actionStep "./.github/actions/deployment-doc-peridot" M.empty
