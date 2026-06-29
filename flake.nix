f6740ed8e23020a11a715f8168a3792ec1464bd5
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2026-06-29 13:29:34 +0900

    fix: update nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "bedrock devenv";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs =
     { nixpkgs, ... }:
