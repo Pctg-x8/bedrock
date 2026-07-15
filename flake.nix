380523f95930be9f75616d2103404842fd5dc917
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2026-07-15 23:52:56 +0900

    fix: devshells per platform

--- flake.nix
+++ flake.nix
@@ -7,40 +7,30 @@
   outputs =
     { nixpkgs, ... }:
     let
-      targetSystems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-      common-deps = pkgs: [
-        pkgs.rustup
-        pkgs.vulkan-loader
-        # required for workflow generator(also included in githooks)
-        pkgs.stack
-        # debugging
-        pkgs.vulkan-validation-layers
-        # git hook
-        (rusty-hook pkgs)
-      ];
-      rusty-hook =
+      devshell =
         pkgs:
-        pkgs.rustPlatform.buildRustPackage rec {
-          pname = "rusty-hook";
-          version = "0.11.2";
-          cargoHash = "sha256-HC+1Cs2BeIPHuuGxcFEB8GqcyrrUEYcSM+KgE/INxIw=";
-          src = pkgs.fetchFromGitHub {
-            owner = "swellaby";
-            repo = pname;
-            rev = "30162426ec354d70ffdb91d1fe93549a58770c3d"; # master at 2025/12/31
-            hash = "sha256-enqEsI0TSazVpIP9Awt/ZWjbxE6j1zzccggLF4SF358=";
-          };
-        };
-    in
-    builtins.foldl' (a: b: a // b) { } (
-      map (
-        system:
         let
-          pkgs = import nixpkgs { inherit system; };
-          deps = common-deps pkgs;
+          common-deps = [
+            pkgs.rustup
+            pkgs.vulkan-loader
+            # required for workflow generator(also included in githooks)
+            pkgs.stack
+            # debugging
+            pkgs.vulkan-validation-layers
+            # git hook
+            rusty-hook
+          ];
+          rusty-hook = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "rusty-hook";
+            version = "0.11.2";
+            cargoHash = "sha256-HC+1Cs2BeIPHuuGxcFEB8GqcyrrUEYcSM+KgE/INxIw=";
+            src = pkgs.fetchFromGitHub {
+              owner = "swellaby";
+              repo = pname;
+              rev = "30162426ec354d70ffdb91d1fe93549a58770c3d"; # master at 2025/12/31
+              hash = "sha256-enqEsI0TSazVpIP9Awt/ZWjbxE6j1zzccggLF4SF358=";
+            };
+          };
           fishPrehook = pkgs.writeScriptBin "startup" ''
             # prepend devenv prompt
             functions -c fish_prompt __fish_prompt_org
@@ -54,18 +44,19 @@
           '';
         in
         {
-          devShells."${system}" = {
-            default = pkgs.mkShell {
-              nativeBuildInputs = deps;
-            };
-            fish = pkgs.mkShell {
-              nativeBuildInputs = deps ++ [ pkgs.fish ];
-              shellHook = ''
-                exec ${pkgs.fish.outPath}/bin/fish -C "source ${fishPrehook}/bin/startup"
-              '';
-            };
+          default = pkgs.mkShell {
+            nativeBuildInputs = common-deps;
           };
-        }
-      ) targetSystems
-    );
+          fish = pkgs.mkShell {
+            nativeBuildInputs = common-deps ++ [ pkgs.fish ];
+            shellHook = ''
+              exec ${pkgs.fish.outPath}/bin/fish -C "source ${fishPrehook}/bin/startup"
+            '';
+          };
+        };
+    in
+    {
+      devShells.x86_64-linux = devshell (import nixpkgs { system = "x86_64-linux"; });
+      devShells.aarch64-darwin = devshell (import nixpkgs { system = "aarch64-darwin"; });
+    };
 }
