e120475438b8811d2c1fff8a3cb70cd3eb373a2c
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2026-07-19 09:16:16 +0900

    rm: rusty-hook

--- flake.nix
+++ flake.nix
@@ -17,20 +17,7 @@
             pkgs.stack
             # debugging
             pkgs.vulkan-validation-layers
-            # git hook
-            rusty-hook
           ];
-          rusty-hook = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "rusty-hook";
-            version = "0.11.2";
-            cargoHash = "sha256-HC+1Cs2BeIPHuuGxcFEB8GqcyrrUEYcSM+KgE/INxIw=";
-            src = pkgs.fetchFromGitHub {
-              owner = "swellaby";
-              repo = pname;
-              rev = "30162426ec354d70ffdb91d1fe93549a58770c3d"; # master at 2025/12/31
-              hash = "sha256-enqEsI0TSazVpIP9Awt/ZWjbxE6j1zzccggLF4SF358=";
-            };
-          };
           fishPrehook = pkgs.writeScriptBin "startup" ''
             # prepend devenv prompt
             functions -c fish_prompt __fish_prompt_org
